3a61e8d889284bfc46ccf490839f731700f24c87
liuzdz <liuzdz@163.com>
2025-03-27 15:53:29 +0800

    初始化个人插件
    初始化个人插件

--- plugins.v2/alistcopy/__init__.py
+++ plugins.v2/alistcopy/__init__.py
@@ -14,19 +14,19 @@ from app.utils.http import RequestUtils
 
 class ChineseSubFinder(_PluginBase):
     # 插件名称
-    plugin_name = "ChineseSubFinder"
+    plugin_name = "目录实时监控-alist复制"
     # 插件描述
-    plugin_desc = "整理入库时通知ChineseSubFinder下载字幕。"
+    plugin_desc = "监控目录文件变化，自动复制媒体文件至alist目录。"
     # 插件图标
     plugin_icon = "chinesesubfinder.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.0.0"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "liuzdz"
     # 作者主页
-    author_url = "https://github.com/jxxghp"
+    author_url = "https://github.com/liuzdz"
     # 插件配置项ID前缀
-    plugin_config_prefix = "chinesesubfinder_"
+    plugin_config_prefix = "alistcopy_"
     # 加载顺序
     plugin_order = 5
     # 可使用的用户级别
@@ -214,41 +214,4 @@ class ChineseSubFinder(_PluginBase):
             # 路径替换
             if self._local_path and self._remote_path and file_path.startswith(self._local_path):
                 file_path = file_path.replace(self._local_path, self._remote_path).replace('\\', '/')
-
-            # 调用CSF下载字幕
-            self.__request_csf(req_url=req_url,
-                               file_path=file_path,
-                               item_type=0 if item_type == MediaType.MOVIE else 1,
-                               item_bluray=item_bluray)
-
-    @lru_cache(maxsize=128)
-    def __request_csf(self, req_url, file_path, item_type, item_bluray):
-        # 一个名称只建一个任务
-        logger.info("通知ChineseSubFinder下载字幕: %s" % file_path)
-        params = {
-            "video_type": item_type,
-            "physical_video_file_full_path": file_path,
-            "task_priority_level": 3,
-            "media_server_inside_video_id": "",
-            "is_bluray": item_bluray
-        }
-        try:
-            res = RequestUtils(headers={
-                "Authorization": "Bearer %s" % self._api_key
-            }).post(req_url, json=params)
-            if not res or res.status_code != 200:
-                logger.error("调用ChineseSubFinder API失败！")
-            else:
-                # 如果文件目录没有识别的nfo元数据， 此接口会返回控制符，推测是ChineseSubFinder的原因
-                # emby refresh元数据时异步的
-                if res.text:
-                    job_id = res.json().get("job_id")
-                    message = res.json().get("message")
-                    if not job_id:
-                        logger.warn("ChineseSubFinder下载字幕出错：%s" % message)
-                    else:
-                        logger.info("ChineseSubFinder任务添加成功：%s" % job_id)
-                elif res.status_code != 200:
-                    logger.warn(f"ChineseSubFinder调用出错：{res.status_code} - {res.reason}")
-        except Exception as e:
-            logger.error("连接ChineseSubFinder出错：" + str(e))
+            logger.info("目录实时监控-alist复制，转换后的源地址为: %s" % file_path)
